18b62b6cdaad2b665aa0c1df1e88a4dac195753d
Dylan <99437817+gainesd2@users.noreply.github.com>
2022-11-06 02:16:12 -0500

    Final Base Project
    Refactored LocationScreen to be a Listview that displays the same initial view, but can be scrolled to display any widgets added to the list.
    -- This list is marked with a TODO comment
    
    Refactored the widget_test file to test for the loading screens load. Purely functions as an example.

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -47,84 +47,128 @@ class _LocationScreenState extends State<LocationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('images/location_background.jpg'),
-            fit: BoxFit.cover,
-            colorFilter: ColorFilter.mode(
-                Colors.white.withOpacity(0.8), BlendMode.dstATop),
-          ),
-        ),
-        constraints: BoxConstraints.expand(),
-        child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return Container(
+            decoration: buildBoxDecoration(),
+            constraints: BoxConstraints.expand(),
+            child: SafeArea(
+              child: ListView(
+                // the scrollable list of widgets
                 children: <Widget>[
-                  TextButton(
-                    onPressed: () async {
-                      var weatherData = await weather.getLocationWeather();
-                      updateUI(weatherData);
-                    },
-                    child: Icon(
-                      Icons.near_me,
-                      size: 50.0,
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () async {
-                      var typedName = await Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return CityScreen();
-                          },
-                        ),
-                      );
-                      if (typedName != null) {
-                        var weatherData =
-                            await weather.getCityWeather(typedName);
-                        updateUI(weatherData);
-                      }
-                    },
-                    child: Icon(
-                      Icons.location_city,
-                      size: 50.0,
-                    ),
-                  ),
+                  buildBaseAppScreen(constraints, context),
+                  // TODO widgets/widget-returning-methods go here
+                  buildExampleWidget(context),
                 ],
               ),
-              Padding(
-                padding: EdgeInsets.only(left: 15.0),
-                child: Row(
-                  children: <Widget>[
-                    Text(
-                      '$temperature°',
-                      style: kTempTextStyle,
-                    ),
-                    Text(
-                      weatherIcon,
-                      style: kConditionTextStyle,
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(right: 15.0),
-                child: Text(
-                  '$weatherMessage in $cityName',
-                  textAlign: TextAlign.right,
-                  style: kMessageTextStyle,
-                ),
-              ),
-            ],
+            ),
+          );
+        }, // end of builder
+      ),
+    );
+  }
+
+  Container buildExampleWidget(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(5.0),
+      color: Theme.of(context).backgroundColor,
+      child: Placeholder(
+        color: Theme.of(context).primaryColor,
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Text(
+            'Placeholder for New Widgets',
+            style: kMessageTextStyle,
           ),
         ),
       ),
+    );
+  }
+
+  BoxDecoration buildBoxDecoration() {
+    return BoxDecoration(
+      image: DecorationImage(
+        image: AssetImage('images/location_background.jpg'),
+        fit: BoxFit.cover,
+        colorFilter:
+            ColorFilter.mode(Colors.white.withOpacity(0.8), BlendMode.dstATop),
+      ),
+    );
+  }
+
+  Widget buildBaseAppScreen(BoxConstraints constraints, BuildContext context) {
+    return Container(
+      constraints: BoxConstraints.expand(height: constraints.maxHeight),
+      // Initial View (looks like base app)
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          // "AppBar" that's actually just a row at the top of the screen
+          buildFakeAppBar(context),
+          Padding(
+            padding: EdgeInsets.only(left: 15.0),
+            child: Row(
+              children: <Widget>[
+                Text(
+                  '$temperature°',
+                  style: kTempTextStyle,
+                ),
+                Text(
+                  weatherIcon,
+                  style: kConditionTextStyle,
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: 15.0),
+            child: Text(
+              '$weatherMessage in $cityName',
+              textAlign: TextAlign.right,
+              style: kMessageTextStyle,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Row buildFakeAppBar(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        TextButton(
+          onPressed: () async {
+            var weatherData = await weather.getLocationWeather();
+            updateUI(weatherData);
+          },
+          child: Icon(
+            Icons.near_me,
+            size: 50.0,
+          ),
+        ),
+        TextButton(
+          onPressed: () async {
+            var typedName = await Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return CityScreen();
+                },
+              ),
+            );
+            if (typedName != null) {
+              var weatherData = await weather.getCityWeather(typedName);
+              updateUI(weatherData);
+            }
+          },
+          child: Icon(
+            Icons.location_city,
+            size: 50.0,
+          ),
+        ),
+      ],
     );
   }
 }
