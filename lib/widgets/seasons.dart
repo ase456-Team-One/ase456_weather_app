4a52a6cbea03c846b69cd3bfa587cf0440a1753c
wolla22 <95152638+wolla22@users.noreply.github.com>
2022-12-04 18:59:37 -0500

    Comments added

--- lib/widgets/seasons.dart
+++ lib/widgets/seasons.dart
@@ -12,39 +12,47 @@ class Seasons extends StatefulWidget {
 }
 
 class _SeasonsState extends State<Seasons> {
-  Icon getSeason() {
-    DateTime date = DateTime.now();
-    DateFormat formatter = DateFormat('MM-dd-yy');
-    String formatDate = formatter.format(date);
-    final date_array = formatDate.split('-');
-    print(date_array);
-    if (int.parse(date_array[0]) >= 3 && int.parse(date_array[0]) < 6) {
+  //Parameter is the month determined by DateTime.now()
+  Icon monthPicker(String month) {
+    //Determine what range of months the value for month lies within and return an Icon specific to the season which those months represent
+    if (int.parse(month) >= 3 && int.parse(month) < 6) {
       return Icon(
         Icons.filter_vintage_rounded,
         color: Colors.lightGreen,
         size: 40.0,
       );
-    } else if (int.parse(date_array[0]) >= 6 && int.parse(date_array[0]) < 9) {
+    } else if (int.parse(month) >= 6 && int.parse(month) < 9) {
       return Icon(
         Icons.sunny,
-        color: Colors.orange.shade600,
-        size: 26.0,
+        color: Colors.orange,
+        size: 40.0,
       );
-    } else if (int.parse(date_array[0]) >= 9 && int.parse(date_array[0]) < 12) {
+    } else if (int.parse(month) >= 9 && int.parse(month) < 12) {
       return Icon(
         Icons.forest_rounded,
         color: Colors.green,
-        size: 26.0,
+        size: 40.0,
       );
     } else {
       return Icon(
         Icons.ac_unit_rounded,
         color: Colors.white,
-        size: 26.0,
+        size: 40.0,
       );
     }
   }
 
+  //Called by the build function to determine the current date
+  Icon getSeason() {
+    DateTime date = DateTime.now();
+    DateFormat formatter = DateFormat('MM-dd-yy');
+    String formatDate = formatter.format(date);
+    //Turn string for date retrieved by DateTime.now() into array
+    final date_array = formatDate.split('-');
+    //Pass the month value (index 0 in date_array) to monthPicker()
+    return monthPicker(date_array[0]);
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Container(
